b15c03d019acd079c2d2988aa48104a2b11f3de7
Kisha <Kisha@fizzburg.net>
2012-12-10 16:08:13 -0500

    determined number of duplicates

--- test.py
+++ test.py
@@ -9,30 +9,42 @@ f=open(data,'rU')
 def get_data():
     read_data=csv.DictReader(f,fieldnames=None)
     raw_leads = list(read_data)
+    print len(raw_leads), "leads"
     return raw_leads
 
 
-def find_duplicate_emails(raw_leads):
+def get_unique_emails(raw_leads):
     emails = {}
     for row in raw_leads:
         if row['Email'] in emails:
             emails[row['Email']] += 1
         else:
             emails[row['Email']] = 1
+    print len(emails), "unique email addresses within", sum(emails.itervalues()), "leads"
     return emails
     
-def update_status(raw_leads,emails):
+def find_duplicate_emails(raw_leads,emails):
     duplicate_emails = []
     for key, value in emails.iteritems():
         if value > 1:
             duplicate_emails.append([key, value])
-    print len(duplicate_emails),    
-    return duplicate_emails, len(duplicate_emails)
+    print len(duplicate_emails), "emails are duplicates"
+    return duplicate_emails
+
+def test(duplicate_emails):
+    list_count = 0
+    for list in duplicate_emails:
+        list_count += list[1]
+    print "duplicate emails appear", float(list_count)/len(duplicate_emails), "times, on average"
+    return list_count
+
+
 
 def main():
     raw_leads = get_data()
-    emails = find_duplicate_emails(raw_leads)
-    update_status(raw_leads,emails)
+    emails = get_unique_emails(raw_leads)
+    duplicate_emails = find_duplicate_emails(raw_leads,emails)
+    print test(duplicate_emails)
 
 if __name__ == "__main__":
 
